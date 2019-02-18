a31887134cbdc57a3b77c7457a21d49d579696e8
JULIO FORTUNA <jcfort@globo.com>
2019-02-18 19:20:53 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sistema de vendas WEB MVC C#";
+            ViewData["Professor"] = "Nelio Alves";
 
             return View();
         }
